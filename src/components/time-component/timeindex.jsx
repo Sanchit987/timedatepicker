f43f05f36bd3041fa6ede408a02441cf6871a68d
Sanchit987 <kherasanchit1@gmail.com>
2022-03-21 13:57:01 +0530

    Completed the time picker

--- src/components/time-component/timeindex.jsx
+++ src/components/time-component/timeindex.jsx
@@ -3,20 +3,36 @@ import Counter from "./counter";
 
 
 const TimeIndex = ({ value, onChange }) => {
+  // const [x, setx] = useState(0);
   let currMoment = value.clone();
+
+  // function settingX() {
+  //   if (x === 0) {
+  //     setx(1);
+  //   } else {
+  //     setx(0);
+  //   }
+  //   if (x === 0) {
+  //     onChange(value.clone());
+  //   } else {
+  //     onChange(value.clone());
+  //   }
+  // }
+
+
   return (<div className="time-selector">
-    <div className="header">{currMoment.format("hh:mm A").toString()}</div>
+    <div className="header">{currMoment.format("HH:mm").toString()}</div>
 
     <div className="time_updater">
       <div className="time-ch">
-        <Counter value={value} onChange={onChange} what='h' />
+        <Counter value={value} onChange={onChange} what='H' />
         <div>:</div>
         <Counter value={value} onChange={onChange} what='m' />
       </div>
-      <div className="day-time">
-        <div className="morning">AM</div>
-        <div className="noon">PM</div>
-      </div>
+      {/* <div className="day-time">
+        <div onClick={settingX} className={`morning ${x ? "day-col" : ""}`}>AM</div>
+        <div onClick={settingX} className={`noon ${x ? "" : "day-col"}`} >PM</div>
+      </div> */}
     </div>
   </div>);
 };
